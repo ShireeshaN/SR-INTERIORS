413af3637ca0d3c479ecc302f22123f6cc346779
Patternscompany <pandureddypatterns@gmail.com>
2024-10-15 13:58:12 +0530

    pp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,25 @@
 
 
 $(document).ready(function() {
-$(".owl-carousel-section1").owlCarousel({
+ // Pricing carousel
+ $(".pricing-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
     loop: true,
-    margin: 15,
-    items: 2,
-    lazyLoad: true,
     dots: false,
-    itemsMobile: [320, 2],
-    nav: true,
-    navText: [
-        '<img src="images/ic_previous_1.png"/>',
-        '<img src="images/ic_next.png"/>',
-    ],
+    nav: false,
     responsive: {
-        0: {
-            items: 1,
-        },
-        320: {
-            items: 1,
-            loop: true,
-            autoHeight: false,
-            nav: false,
-            dots: true,
-            margin: 5,
-        },
-        480: {
-            items: 1,
-            loop: true,
-            autoHeight: false,
-            nav: false,
-            dots: true,
-            margin: 0,
-        },
-        768: {
-            items: 1,
-            loop: true,
-            autoHeight: false,
-            nav: false,
-            dots: true,
-            margin: 5,
-        },
-        1000: {
-            items: 3,
-        },
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
     },
-})
+  });
+
 })
