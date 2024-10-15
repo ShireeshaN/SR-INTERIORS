6c849f05c9184441bbbc38292c91c413fe62780a
Patternscompany <pandureddypatterns@gmail.com>
2024-10-15 14:31:14 +0530

    pp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,35 @@
 
 
 $(document).ready(function() {
- // Pricing carousel
- $(".pricing-carousel").owlCarousel({
+// Pricing carousel
+$(".pricing-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: [
+      `<i class="fa fa-arrow-left"></i>`,  // Left arrow
+      `<i class="fa fa-arrow-right"></i>`  // Right arrow
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+  
+ $(".services").owlCarousel({
     autoplay: true,
     smartSpeed: 1500,
     margin: 30,
@@ -18,6 +45,9 @@ $(document).ready(function() {
       },
       768: {
         items: 2,
+      },
+      1200: {
+        items: 3,
       },
     },
   });
