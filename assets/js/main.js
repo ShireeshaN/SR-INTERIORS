ac133b7841381338e6b51460a0c6589c01a89fb8
Patternscompany <pandureddypatterns@gmail.com>
2025-01-27 11:03:25 +0530

    pp

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,58 @@ $(".pricing-carousel").owlCarousel({
       },
     },
   });
+$(".gallery-b-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: true,
+    nav: true,
+    navText: [
+      `<i class="fa fa-arrow-left"></i>`,  // Left arrow
+      `<i class="fa fa-arrow-right"></i>`  // Right arrow
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+$(".testi-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: true,
+    nav: true,
+    navText: [
+      `<i class="fa fa-arrow-left"></i>`,  // Left arrow
+      `<i class="fa fa-arrow-right"></i>`  // Right arrow
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
   
  $(".services").owlCarousel({
     autoplay: true,
