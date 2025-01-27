7b717606f0e1e80bfc7a2515c8ee477d568da84a
Patternscompany <pandureddypatterns@gmail.com>
2025-01-27 15:36:05 +0530

    pp

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,6 +80,37 @@ $(".testi-carousel").owlCarousel({
       },
     },
   });
+  $(".up-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000, // Time between slides
+    autoplayHoverPause: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: true,
+    nav: true,
+    navText: [
+      `<i class="fa fa-arrow-left"></i>`,  // Left arrow
+      `<i class="fa fa-arrow-right"></i>`  // Right arrow
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+    animateOut: 'slideOutUp', // Custom animation for items leaving
+    animateIn: 'slideInUp',   // Custom animation for items entering
+  });
+  
   
  $(".services").owlCarousel({
     autoplay: true,
